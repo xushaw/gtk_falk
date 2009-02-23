ed05b68ef686d1ec4dd57efdf60834ec8c87e0ce
xushaw <xubunta@gmail.com>
2009-02-23 13:48:08 +0300

    +menu

--- text-ent.c
+++ text-ent.c
@@ -9,6 +9,8 @@
   entry_text = gtk_entry_get_text (GTK_ENTRY (entry));
   printf ("Entry contents: %s\n", entry_text);
 }*/
+static void menuitem_response (gchar *);
+
 static void sch_callback( GtkWidget *widget, GtkWidget *entry ) //1-й арг - кто отсылает сигнал
 {
     const gchar *entry_text;
@@ -31,18 +33,47 @@ int main( int   argc,
     GtkWidget *notebook;
     GtkWidget *label1, *label2;
     GtkWidget *frame1, *frame2, *frame3, *frame4, *frame5;
+    GtkWidget *menu;
+    GtkWidget *menu_bar;
+    GtkWidget *root_menu;
+    GtkWidget *menu_items;
+    char buf[512];
+    int i;
     
     gtk_init (&argc, &argv);
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_widget_set_size_request (GTK_WIDGET (window), 500, 150); //200 100
-    gtk_window_set_title (GTK_WINDOW (window), "pam-pam");
+    gtk_widget_set_size_request (GTK_WIDGET (window), 800, 300); //500 150
+    gtk_window_set_title (GTK_WINDOW (window), "выбор");
     g_signal_connect (G_OBJECT (window), "destroy",
                       G_CALLBACK (gtk_main_quit), NULL);
     g_signal_connect_swapped (G_OBJECT (window), "delete_event",
                               G_CALLBACK (gtk_widget_destroy), 
                               G_OBJECT (window));
     notebook = gtk_notebook_new ();
+//-----------------------------------------------    
+    menu = gtk_menu_new ();
+
+    for (i=0; i<3; i++)
+    {
+        sprintf(buf, "Подменю-%d", i);
+        menu_items = gtk_menu_item_new_with_label (buf);
+        gtk_menu_shell_append (GTK_MENU_SHELL (menu), menu_items);//add menu-item to menu
+
+        g_signal_connect_swapped (G_OBJECT (menu_items), "activate",
+                                  G_CALLBACK (menuitem_response),  (gpointer) g_strdup (buf));
+        gtk_widget_show (menu_items);
+    }
+    root_menu = gtk_menu_item_new_with_label ("Root Menu");
+    gtk_widget_show (root_menu);
+
+    gtk_menu_item_set_submenu (GTK_MENU_ITEM (root_menu), menu);
+
+    menu_bar = gtk_menu_bar_new ();
+//    gtk_box_pack_start (GTK_BOX (vbox1), menu_bar, FALSE, FALSE, 2);//2?
+    gtk_widget_show (menu_bar);
+
+    gtk_menu_shell_append (GTK_MENU_SHELL (menu_bar), root_menu);
 
     x_hbox = gtk_hbox_new (FALSE, 0);
     g_hbox = gtk_hbox_new (FALSE, 0);
@@ -91,11 +122,6 @@ int main( int   argc,
          button_sch = gtk_button_new_with_label ("Поиск"); 
          button_clr = gtk_button_new_with_label ("Сброс");
 
-//         g_signal_connect (G_OBJECT (button_sch), "clicked",
-//                           G_CALLBACK (callback), (gpointer) "sch clicked");
-//         g_signal_connect (G_OBJECT (entry1), "activate",
-//                           G_CALLBACK (sch_callback), (gpointer) entry1);
-
          g_signal_connect (G_OBJECT (button_sch), "clicked",
                            G_CALLBACK (sch_callback), (gpointer) entry1);
 
@@ -119,19 +145,6 @@ g_signal_connect (G_OBJECT (button_clr), "clicked",
 g_signal_connect (G_OBJECT (button_clr), "clicked",
                            G_CALLBACK (clr_callback), (gpointer) entry5);
 
-
-/*       g_signal_connect (G_OBJECT (entry2), "activate",
-                           G_CALLBACK (enter_callback),
-                           (gpointer) entry2);
-         g_signal_connect (G_OBJECT (entry3), "activate",
-                           G_CALLBACK (enter_callback),
-                           (gpointer) entry3);
-         g_signal_connect (G_OBJECT (entry4), "activate",
-                           G_CALLBACK (enter_callback),
-                           (gpointer) entry4);
-         g_signal_connect (G_OBJECT (entry5), "activate",
-                           G_CALLBACK (enter_callback),
-                           (gpointer) entry5);*/
          vbox1 = gtk_vbox_new (FALSE, 0);
          vbox2 = gtk_vbox_new (FALSE, 0);
          bhbox = gtk_hbox_new (FALSE, 0);
@@ -143,6 +156,9 @@ g_signal_connect (G_OBJECT (button_clr), "clicked",
          frame5 = gtk_frame_new (NULL);
 
     gtk_container_add (GTK_CONTAINER (x_hbox), vbox1);
+    
+    gtk_box_pack_start (GTK_BOX (vbox1), menu_bar, FALSE, FALSE, 2);//2?
+
     gtk_container_add (GTK_CONTAINER (x_hbox), vbox2);
     gtk_container_add (GTK_CONTAINER (g_hbox), vbox2);
     gtk_container_add (GTK_CONTAINER (vbox1), frame1);
@@ -179,10 +195,8 @@ g_signal_connect (G_OBJECT (button_clr), "clicked",
 //    gtk_container_add (GTK_CONTAINER (align), entry3);
 //    gtk_container_add (GTK_CONTAINER (align), entry4);
 //    gtk_container_add (GTK_CONTAINER (align), button_sch); 
-
 //    align = gtk_alignment_new (10, 10, 20, 20);
 //    gtk_widget_show (align);
-
     gtk_box_pack_start (GTK_BOX (x_hbox),  vbox1, FALSE, FALSE, 0); 
     gtk_box_pack_start (GTK_BOX (x_hbox),  vbox2, FALSE, FALSE, 0); 
     gtk_box_pack_start (GTK_BOX (g_hbox),  vbox2, FALSE, FALSE, 0); 
@@ -192,17 +206,6 @@ g_signal_connect (G_OBJECT (button_clr), "clicked",
     gtk_box_pack_start (GTK_BOX (vbox2), entry4, FALSE, FALSE, 0);
     gtk_box_pack_start (GTK_BOX (vbox1), entry5, FALSE, FALSE, 0);
     gtk_box_pack_start (GTK_BOX (vbox2), button_sch, FALSE, FALSE, 0);
-/*---------------------button-"close"----------------------------v
-
-    button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-    g_signal_connect_swapped (G_OBJECT (button), "clicked",
-			      G_CALLBACK (gtk_widget_destroy),
-			      G_OBJECT (window));
-    gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
-    GTK_WIDGET_SET_FLAGS (button, GTK_CAN_DEFAULT);
-    gtk_widget_grab_default (button);
-    gtk_widget_show (button); 
-//---------------------button-"close"----------------------------*/
     gtk_widget_show (entry1);
     gtk_widget_show (entry2);
     gtk_widget_show (entry3);
@@ -226,4 +229,9 @@ g_signal_connect (G_OBJECT (button_clr), "clicked",
     gtk_main();
 
     return 0;
+}
+
+static void menuitem_response( gchar *string )
+{
+        printf ("%s\n", string);
 }
